44a312b66c075fa20680737f5deb54a710eefa4b
RAVENPRAVIN <prasannapravin76@gmail.com>
2024-11-06 16:54:45 +0530

    completed

---
+++ program & Output/server.py
@@ -0,0 +1,22 @@
+#server.py
+import socket                    
+port = 60000                    
+s = socket.socket()              
+host = socket.gethostname()      
+s.bind((host, port))              
+s.listen(5)                      
+while True: 
+    conn, addr = s.accept()      
+    data = conn.recv(1024) 
+    print('Server received', repr(data)) 
+    filename='pravin.txt' 
+    f = open(r'C:\Users\pravi\OneDrive\Desktop\COURSES\Computer Network\EXPERIMENTS\3c.FILE_TRANSFER_USING_TCP_SOCKETS\program & Output\pravin.txt','rb') 
+    l = f.read(1024) 
+    while (l): 
+       conn.send(l) 
+       print('Sent ',repr(l)) 
+       l = f.read(1024) 
+    f.close() 
+    print('Done sending') 
+    conn.send('Thank you for connecting'.encode()) 
+    conn.close()
---
+++ program & Output/tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+#server.py
+import socket                    
+port = 60000                    
+s = socket.socket()              
+host = socket.gethostname()      
+s.bind((host, port))              
+s.listen(5)                      
+while True: 
+    conn, addr = s.accept()      
